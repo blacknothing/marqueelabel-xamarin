01c4f5062d04cb2e0538827b813061997c4df434
Dave Hanson <dave.hanson@audioaddict.com>
2016-03-09 08:13:28 -0600

    Fixing ARM64 support of the .a library.

--- MarqueeLabelSDK/libMarqueeLabelSDK.linkwith.cs
+++ MarqueeLabelSDK/libMarqueeLabelSDK.linkwith.cs
@@ -1,3 +1,3 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("libMarqueeLabelSDK.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("libMarqueeLabelSDK.a", SmartLink = true, ForceLoad = true)]
